c04b66238a53683e391755948abdde7b7ac748b9
pchawin40 <pathocha000@gmail.com>
2023-08-28 00:02:12 -0700

    Added initial files for django, currently trying to figure out postgresql | 12:02 AM 8.28.23 - CP

---
+++ odyso/odyso_safety_security/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OdysoSafetySecurityConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'odyso_safety_security'
---
+++ odyso/odyso_users/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OdysoUsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'odyso_users'
