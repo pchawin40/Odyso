ffd84d32db632753b4c5853ba48fa502c37fee1a
pchawin40 <pathocha000@gmail.com>
2023-08-29 21:32:06 -0700

    Registered model for recommendations, safety, itineraries | 9:32 PM 8.29.23 - CP

--- odyso/odyso_recommendations/admin.py
+++ odyso/odyso_recommendations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import odyso_recommendations, odyso_safety_security, odyso_itineraries
 
-# Register your models here.
+# register models
+admin.site.register(odyso_recommendations)
+admin.site.register(odyso_safety_security)
+admin.site.register(odyso_itineraries)
